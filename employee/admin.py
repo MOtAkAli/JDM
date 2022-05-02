c30e0b0f0ed8585f701c34df0ac584dc3e5643fa
voidnowhere <alivnw@outlook.com>
2022-05-02 03:42:14 +0000

    registering models for django admin

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Employee, EmployeeLog, CarBrand, CarType, CarModel, Agency, Car, Reservation, City
 
-# Register your models here.
+admin.site.register(City)
+admin.site.register(Employee)
+admin.site.register(EmployeeLog)
+admin.site.register(CarBrand)
+admin.site.register(CarModel)
+admin.site.register(CarType)
+admin.site.register(Agency)
+admin.site.register(Car)
+admin.site.register(Reservation)
