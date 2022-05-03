87f951cf6ec0ac51ebb8fd9cac4a1ca41444885d
Abdelmoughit Moubarik <moughitmoubarik08@gmail.com>
2022-05-03 23:49:21 +0000

    select city option come from db table : Citys add view code to insert Citys in select in register.html

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User, auth
-from .models import *
 from .forms import ClientForm
+from employee.models import City
+
 
 def index(request):
     return render(request, 'home/index.html')
@@ -19,11 +20,14 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
+        password1 = request.POST['password']
+        password2 = request.POST['passwordc']
         form = ClientForm(request.POST)
         print(form.errors)
-        if form.is_valid():
+        if form.is_valid() and password1 == password2:
             form.save()
             print("saved")
         return redirect('/')
     else:
-        return render(request, 'home/register.html')
+        results = City.objects.all
+        return render(request, 'home/register.html', {"Citys": results})
