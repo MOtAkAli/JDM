f5ea5ada06c03d3f33030a1e4c574291661a87ee
voidnowhere <alivnw@outlook.com>
2022-05-08 21:41:29 +0100

    fully functioned users view

--- employee/urls.py
+++ employee/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    re_path(r'^users/(?P<setof>(10|25|50|100))/(?P<num_page>[1-9]\d*)', views.users, name="users"),
+    path('', views.index, name='index'),
+    re_path(r'^users/search/(?P<search>((id|first_name|last_name|email|phone)=(\w+[\-\.]*\w+@?\w+\-?\w+\.?\w+|\w+))?)/(?P<setof>(10|25|50|100))/(?P<num_page>[1-9]\d*)', views.users, name='users'),
 ]
