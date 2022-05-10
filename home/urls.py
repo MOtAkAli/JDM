af7cd8078c76def1628e3a868fbcc06ada3122bd
Abdelmoughit Moubarik <moughitmoubarik08@gmail.com>
2022-05-11 00:09:12 +0100

    commit on "main"

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("rent/", views.rent, name="rent"),
+    path('rent/', views.CarListView.as_view(), name='rent'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render
-from django.utils import timezone
 from django.views.generic.list import ListView
+from employee.models import Car, Agency,CarModel,CarBrand,CarType
+from employee.filters import CarFilter
 
-from articles.models import Article
+class CarListView(ListView):
 
-class ArticleListView(ListView):
-
-    model = Article
-    paginate_by = 100  # if pagination is desired
+    model = Car
+    template_name = 'home/cars.html'
+    paginate_by = 9  # if pagination is desired
+    context_object_name = 'cars'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['now'] = timezone.now()
-        return context
+        context["brands"] = CarBrand.objects.all()
+        context["types"] = CarType.objects.all()
+        context["CarModel"] = CarModel.objects.all()
+
+        context.update(
+            filtered_cars=CarFilter(self.request.GET, queryset=self.model.objects.all())
+        )
+        context['title'] = ' Rent a car '
+        print(context['filtered_cars'].qs)
+        return context    
+
+
+    def get_queryset(self):
+        qs = self.model.objects.all().filter(is_active=True,in_use=False)
+        filtered_cars = CarFilter(self.request.GET, queryset=qs)
+        return filtered_cars.qs
 
 def index(request):
     return render(request, 'home/index.html')
-
-
-def rent(request):
-    return render(request, 'home/cars.html')
-
