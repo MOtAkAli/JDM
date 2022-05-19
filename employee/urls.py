a525e6c6759a64c4f968505647d52fb94c08051f
idamou <othmane.idamou@emsi-edu.ma>
2022-05-19 23:02:53 +0100

    reservations management

--- employee/urls.py
+++ employee/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    re_path(r'^users/search/(?P<search>((id|first_name|last_name|email|phone)=(\w+[\-\.]*\w+@?\w+\-?\w+\.?\w+|\w+))?)/(?P<setof>(10|25|50|100))/(?P<num_page>[1-9]\d*)', views.users, name='users'),
+    re_path(
+        r'^users/search/(?P<search>((id|first_name|last_name|email|phone)=(\w+[\-\.]*\w+@?\w+\-?\w+\.?\w+|\w+))?)/(?P<setof>(10|25|50|100))/(?P<num_page>[1-9]\d*)',
+        views.users, name='users'),
+    re_path(
+        r'^reservations/search/(?P<search>((id|first_name|last_name|email|phone)=(\w+[\-\.]*\w+@?\w+\-?\w+\.?\w+|\w+))?)/(?P<setof>(10|25|50|100))/(?P<num_page>[1-9]\d*)',
+        views.reservations, name='reservations'),
+    path("reservation/<int:id>", views.reservation, name="details"),
 ]
