2c0e864fe58e5519a0efd402124f6e00052f5f75
Abdelmoughit Moubarik <moughitmoubarik08@gmail.com>
2022-05-27 09:11:33 +0100

    showing PaymentLog in django admin dashboard

--- employee/admin.py
+++ employee/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EmployeeLog, CarBrand, CarType, CarModel, Agency, Car, Reservation
+from .models import EmployeeLog, CarBrand, CarType, CarModel, Agency, Car, Reservation, PaymentLog
 
 admin.site.register(EmployeeLog)
 admin.site.register(CarBrand)
@@ -8,3 +8,4 @@ admin.site.register(CarType)
 admin.site.register(Agency)
 admin.site.register(Car)
 admin.site.register(Reservation)
+admin.site.register(PaymentLog)
