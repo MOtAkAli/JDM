9dcc21901667c16da6198d9f37fb77096a304aec
Abdelmoughit Moubarik <moughitmoubarik08@gmail.com>
2022-05-27 08:48:21 +0100

    updated .env

--- JDM/settings.py
+++ JDM/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-4n3r^5zz1oggys6nz83utb+ib+w1=i#wqn#g4ibue$#8t*8z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
