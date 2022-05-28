5df9c0690bf719d2acf13ed180f96cb75cec2d69
voidnowhere <alivnw@outlook.com>
2022-05-28 17:40:24 +0100

    import dj-database-url in settings

--- JDM/settings.py
+++ JDM/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
