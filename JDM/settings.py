dc8873e56e31d3b2b4ca7208091652390f3524f9
voidnowhere <alivnw@outlook.com>
2022-05-31 21:38:56 +0100

    .env optimization & migrating media to cloud in production

--- JDM/settings.py
+++ JDM/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -85,9 +87,11 @@ DATABASES = {
     }
 }
 
-# production
-# db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# DATABASES['default'].update(db_from_env)
+PRODUCTION = config('PRODUCTION', cast=bool)
+
+if PRODUCTION:
+    db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -121,9 +125,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# production
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if PRODUCTION:
+    # media files
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': config('CLOUDINARY_API_SECRET'),
+        'API_SECRET': config('CLOUDINARY_API_KEY')
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+    # static files
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
 
@@ -131,17 +145,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# local
 MEDIA_ROOT = BASE_DIR / 'static/base/images'
-
-# production
-# MEDIA_ROOT = BASE_DIR / 'staticfiles/base/images'
-
-# local
 MEDIA_URL = '/images/'
-
-# production
-# MEDIA_URL = 'static/base/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -159,5 +164,5 @@ CITIES_LIGHT_INCLUDE_COUNTRIES = ['MA']
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jdmrent2022@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
