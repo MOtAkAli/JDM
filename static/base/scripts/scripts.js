dc342cc6714cc7afa9601c9c34b59c8d884ecba6
voidnowhere <alivnw@outlook.com>
2022-07-15 21:52:05 +0100

    home index redo & cleanup files & images optimized

--- static/base/scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-    var x = document.getElementById("toast")
-    var message = document.getElementById("desc")
-    x.className = "show";
-    message.textContent = "{{ message }}";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
